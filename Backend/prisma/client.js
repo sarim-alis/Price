6409a8fe7cfc86d7652678681615e2cef0fc0959
sarim-alis <sarimslayerali786@gmail.com>
2025-10-31 02:18:40 +0500

    feat/Backend setup

--- Backend/prisma/client.js
+++ Backend/prisma/client.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 // Singleton pattern to prevent multiple instances in development
 const globalForPrisma = global;
