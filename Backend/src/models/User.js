0a9dc01dff5d5b1433c5dd90143e8920f28e3c85
sarim-alis <sarimslayerali786@gmail.com>
2026-01-18 09:14:11 +0500

    feat: model setup

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name:     { type: String, required: true, trim: true },
-    email:    { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-    role:     { type: String, enum: ["buyer", "seller", "admin"], default: "buyer", index: true },
-    phone:    { type: String }
+    name:         { type: String, required: true, trim: true },
+    email:        { type: String, required: true, unique: true, lowercase: true },
+    password:     { type: String, required: true },
+    role:         { type: String, enum: ["buyer", "seller", "admin"], default: "buyer", index: true },
+    phone:        { type: String },
+    profileImage: { type: String }
   }, { timestamps: true }
 );
 
