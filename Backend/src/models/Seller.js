650caf553d9c00f6caa3cbd05394249824c53a16
sarim-alis <sarimslayerali786@gmail.com>
2026-01-18 09:01:09 +0500

    feat: data modellings

--- Backend/src/models/Seller.js
+++ Backend/src/models/Seller.js
@@ -4,7 +4,7 @@ const sellerSchema = new mongoose.Schema(
   {
     sellerId:   { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
     cnic:       { type: String, required: false, unique: true },
-    document:   { type: String, required: true },
+    document:   { type: String, required: false },
     isVerified: { type: Boolean, default: false },
     verifiedBy:{ type: mongoose.Schema.Types.ObjectId, ref: "User" }
   },
