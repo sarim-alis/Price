74b5faf25e90f2b38280fb7f10eec84b967be0e1
sarim-alis <sarimslayerali786@gmail.com>
2026-03-29 00:33:49 -0700

    feat: model

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -2,10 +2,5 @@ import Mobile from "./Mobile.js";
 import User from "./User.js";
 import Order from "./Order.js";
 import Seller from "./Seller.js";
-import Review from "./Review.js";
-import ReviewSummary from "./ReviewSummary.js";
-import PriceHistory from "./PriceHistory.js";
-import Chat from "./Chat.js";
-import Prediction from "./Prediction.js";
 
-export { Mobile, User, Order, Seller, Review, ReviewSummary, PriceHistory, Chat, Prediction };
+export { Mobile, User, Order, Seller };
