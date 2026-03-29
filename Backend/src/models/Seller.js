5f4b61aa0edc472eabb3a171fa2faf22c6a21e23
sarim-alis <sarimslayerali786@gmail.com>
2026-03-29 12:46:51 -0700

    feat: seller

--- Backend/src/models/Seller.js
+++ Backend/src/models/Seller.js
@@ -5,7 +5,8 @@ const sellerSchema = new mongoose.Schema(
     sellerId:   { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
     cnic:       { type: String, required: true, unique: true },
     shopName:   { type: String, required: true, trim: true },
-    shopPic:  { type: String },
+    shopPic:    { type: String },
+    address:    { type: String, required: true },
   }, { timestamps: true }
 );
 
